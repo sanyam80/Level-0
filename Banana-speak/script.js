b913713ddf532355dce0150954c6091f794d1a99
Sanyam Kangtani <sanyamkangtani803@gmail.com>
2022-10-05 13:00:01 +0530

    final commit

---
+++ Banana-speak/script.js
@@ -0,0 +1,22 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+  var url = "https://api.funtranslations.com/translate/minion.json"
+
+function errorHandler(){
+    alert("something wrong went with the server")
+}
+  function getTranslateURL(text){
+    return url + "?" + "text=" + text;
+  }
+function clickHandler(){
+  var inputText = txtInput.value;
+  fetch(getTranslateURL(inputText))
+  .then(response=>response.json()).then(json=>{
+    var translatedText = json.contents.translated
+    outputDiv.innerText = translatedText})
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
+    
